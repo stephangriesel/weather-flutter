7643a2dcf8b8294ff755ecc884c83cdcabf762a6
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-11-23 10:10:37 +0100

    add weather service

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weasther';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('❌ Failed to load weather data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    // Get permission
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // Fetch location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // List of placemark objects
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    // Extract city name of first placemark
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
