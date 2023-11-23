99c77ec2cc89d2d50ce567a09e8e4c43fb34f75f
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-11-23 14:28:17 +0100

    fix typo & refactor

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -2,45 +2,46 @@ import 'dart:convert';
 
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-
-import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
+import '../models/weather_model.dart';
 
 class WeatherService {
-  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weasther';
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
-    final response = await http
-        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+    final response = await http.get(Uri.parse(
+        '$BASE_URL?q=${Uri.encodeComponent(cityName)}&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('❌ Failed to load weather data');
+      throw Exception('❌ Failed to load weather data with status code: ${response.statusCode}');
     }
   }
 
   Future<String> getCurrentCity() async {
     LocationPermission permission = await Geolocator.checkPermission();
-    // Get permission
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
     }
 
-    // Fetch location
+    if (permission == LocationPermission.deniedForever || !await Geolocator.isLocationServiceEnabled()) {
+      throw Exception('Location permission denied permanently or location services are disabled.');
+    }
+
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
-    // List of placemark objects
-    List<Placemark> placemarks =
-        await placemarkFromCoordinates(position.latitude, position.longitude);
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
 
-    // Extract city name of first placemark
-    String? city = placemarks[0].locality;
-
-    return city ?? "";
+    if (placemarks.isNotEmpty) {
+      return placemarks[0].locality ?? "";
+    } else {
+      throw Exception('No placemarks found for the current location.');
+    }
   }
 }
