ecea591f3446ce5e88c571440d7ec2a0433c210e
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-11-23 12:00:21 +0100

    set up page

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/services/weather_service.dart';
+import '../models/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,44 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // API key
+  final _weatherService = WeatherService('919a99d0a0b54916d53522996e6f8d55');
+  Weather? _weather;
+
+  // Fetch Weather
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // Additional fun features
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_weather?.cityName ?? "loading city name..."),
+          Text("${_weather?.temperature.round()}°C")
+        ],
+      ),
+    ));
   }
 }
